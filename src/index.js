b9f18d57e8a1a55aab20d33b6faf2076b7315c1a
Sharan Sharabinth <ssha0054@student.monash.edu>
2022-09-12 23:51:54 +1000

    Add self assess performance as CustomFieldEdit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
 import Resolver from '@forge/resolver';
+import ForgeUI, { 
+    render,
+    useProductContext,
+    CustomField, 
+    CustomFieldEdit,
+    Option,
+    Select,
+    Text, 
+} from '@forge/ui';
 
 const resolver = new Resolver();
 
@@ -8,5 +17,56 @@ resolver.define('getText', (req) => {
     return 'Hello, world!';
 });
 
-export const handler = resolver.getDefinitions();
+const FieldData = () => {
+    const { extensionContext: {fieldValue}} = useProductContext();
 
+    let result = "";
+    for (let i = 0; i < fieldValue; i++) {
+        result += "*";
+    }
+    return (
+        <Text>{result}</Text>
+    );
+}
+
+export const fieldRenderer = render(
+    <CustomField>
+        <FieldData/>
+    </CustomField>
+);
+
+const View = () => {
+    const {
+        extensionContext: { fieldValue },
+    } = useProductContext();
+
+    return (
+        <CustomField>
+            <Text>
+                {fieldValue}
+            </Text>
+        </CustomField>
+    );
+};
+
+const Edit = () => {
+    const onSubmit = (formValue) => {
+        return formValue.myPerformanceRating;
+    }
+
+    return (
+        <CustomFieldEdit onSubmit={onSubmit} header="How would you rate your performance?" width="medium" >
+            <Select label="Select one of the options below to rate your performance" name="myPerformanceRating">
+                <Option label="Bad" value="Bad" />
+                <Option label="Somewhat Bad" value="Somewhat Bad" />
+                <Option label="Okay" value="Okay" />
+                <Option label="Somewhat Good" value="Somewhat Good" />
+                <Option label="Good" value="Good" />
+            </Select>
+        </CustomFieldEdit>
+    );
+};
+
+export const renderPerformanceFieldView = render(<View />);
+export const renderPerformanceFieldEdit = render(<Edit />);
+export const handler = resolver.getDefinitions();
