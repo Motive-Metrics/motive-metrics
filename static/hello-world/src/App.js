0596a651fc45400b36805a84109a7abd129ab25e
andrwcao <80874259+andrwcao@users.noreply.github.com>
2022-09-18 20:17:26 +1000

    Added graph

--- static/hello-world/src/App.js
+++ static/hello-world/src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { invoke } from '@forge/bridge';
+import Chart from './shared/Chart';
 
 function App() {
     const [data, setData] = useState(null);
@@ -7,10 +8,16 @@ function App() {
     useEffect(() => {
         invoke('getText', { example: 'my-invoke-variable' }).then(setData);
     }, []);
-
+    const xData = ['11/11/21', '11/12/21', '11/13/21', '11/14/21', '11/15/21', '11/16/21', '11/18/21'];
+    const colour='rgba(0, 82, 204)'
+    
     return (
         <div>
             {data ? data : 'Loading...'}
+            <Chart
+                xAxis={xData}
+                colour={colour}
+            />
         </div>
     );
 }
