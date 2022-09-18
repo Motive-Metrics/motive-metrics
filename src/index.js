33e54006aa2dea4214dde10d71ab08249cf3996f
Sharan <80957318+ssha0054@users.noreply.github.com>
2022-09-18 21:54:54 +1000

    Rate Performance Functionality (#1)
    * Add self assess performance as CustomFieldEdit
    
    * Display None on field text if no performance rating present
    
    * Remove dummy customfields
    
    * Rename functions used for performance rating
    
    * Add function to retrieve performance ratings of all issues
    
    * Refactor function to retrieve performance ratings
    
    * Add StatusLozenge for display text for performance rating
    
    * Refactor file structure to have performance ratings code in separate folder
    
    * Modify file structure for performance rating and manifest file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 import Resolver from '@forge/resolver';
+import ForgeUI, { render } from '@forge/ui';
+import api, {route} from '@forge/api'
 
 const resolver = new Resolver();
 
-resolver.define('getText', (req) => {
+resolver.define('getText', async (req) => {
     console.log(req);
+    // get data for self performance ratings
+    const performanceData = await getPerformanceRatings(req);
+    console.log(performanceData);
 
-    return 'Hello, world!';
+    return 'Hello, world!!!!!!';
 });
 
-export const handler = resolver.getDefinitions();
+// Function to get the performance ratings of all issues.
+// A list containing object for each issue {issue key, performanceRating, assignee} will be returned.
+// Will be useful later when working with this data for project page (REMOVE COMMENT LATER)
+const getPerformanceRatings = async function(req) {
+    var jql = `project in (${req.context.extension.project.key})`;
+    const response = await api.asApp().requestJira(route`/rest/api/3/search?${jql}`);
+    const data = await response.json();
 
+    var issuePerformances = [];
+    var assigneeName = null;
+    for (var issue of data.issues) {
+        if ( issue.fields.assignee ) {
+            assigneeName = issue.fields.assignee.displayName;
+        }
+        // I think forge gives an ID of custom fields created, so have to refer by the ID 'customfield_10046', instead of the 'key' used in manifest file (REMOVE COMMENT LATER)
+        issuePerformances.push({
+            "key": issue.key,
+            "performanceRating": issue.fields.customfield_10046,
+            "assignee": assigneeName
+        });
+    }
+    
+    return issuePerformances;
+}
+
+export const handler = resolver.getDefinitions();
