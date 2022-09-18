0a78b94590364cf667956131659a96e0428fd334
Sharan Sharabinth <ssha0054@student.monash.edu>
2022-09-19 01:13:42 +1000

    Update colours for pie chart

--- static/hello-world/src/shared/PieChart.js
+++ static/hello-world/src/shared/PieChart.js
@@ -16,20 +16,18 @@ const PieChart = (props) => {
             label,
             data: props.dataset,
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)',
+                'rgba(242, 58, 58, 0.2)', // Bad -> Red
+                'rgba(245, 164, 59, 0.2)', // Somewhat Bad -> Orange
+                'rgba(219, 216, 213, 0.2)', // Okay -> Grey
+                'rgba(255, 206, 86, 0.2)', // Somwhat Good -> Yellow
+                'rgba(75, 192, 192, 0.2)', // Good -> Green
             ],
             borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)',
+                'rgba(242, 58, 58, 1)', // Bad -> Red
+                'rgba(245, 164, 59, 1)', // Somewhat Bad -> Orange
+                'rgba(219, 216, 213, 1)', // Okay -> Grey
+                'rgba(255, 206, 86, 1)', // Somwhat Good -> Yellow
+                'rgba(75, 192, 192, 1)', // Good -> Green
             ],
             borderWidth: 1,
             },
