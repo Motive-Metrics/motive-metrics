e0ca631e4f63ce3a66ab6cee78df632ec49a339c
Jie <jxiangfan@gmail.com>
2022-09-24 11:29:19 +1000

    auto linting

--- src/motivation.js
+++ src/motivation.js
@@ -1,68 +1,88 @@
-import ForgeUI, { render, CustomField, useProductContext, Text, CustomFieldEdit, Select, Option, useState } from '@forge/ui';
-import Resolver from '@forge/resolver';
-import api, {route} from '@forge/api'
-import { getCustomFieldID } from './index';
+import ForgeUI, {
+  render,
+  CustomField,
+  useProductContext,
+  Text,
+  CustomFieldEdit,
+  Select,
+  Option,
+  useState,
+} from "@forge/ui";
+import Resolver from "@forge/resolver";
+import api, { route } from "@forge/api";
+import { getCustomFieldID } from "./index";
 
 const ViewMotivation = () => {
+  const {
+    extensionContext: { fieldValue },
+  } = useProductContext();
 
-    const {
-        extensionContext: { fieldValue },
-    } = useProductContext();
-    
-    const output = fieldValue === null ? 'None' : fieldValue.myMotivationRating
-    return (
-        <CustomField>
-            <Text>{output}</Text>
-        </CustomField>
-    );
+  const output = fieldValue === null ? "None" : fieldValue.myMotivationRating;
+  return (
+    <CustomField>
+      <Text>{output}</Text>
+    </CustomField>
+  );
 };
 
 const EditMotivation = () => {
+  const onSubmit = (formData) => {
+    formData.motivationScore = false;
+    formData.challenging = true;
+    formData.interesting = true;
+    formData.important = true;
+    formData.innovative = true;
+    formData.difficult = true;
+    formData.easy = false;
+    return formData;
+  };
 
-    const onSubmit = (formData) => {
-        formData.motivationScore = false;
-        formData.challenging = true;
-        formData.interesting = true;
-        formData.important = true;
-        formData.innovative = true;
-        formData.difficult = true;
-        formData.easy = false;
-        return formData;
-    };
-      
-    return (
-        <CustomFieldEdit onSubmit={onSubmit} header="Tell us about your motivation?" width="medium" >
-            <Select label="Rate how motivated you were" name="myMotivationRating">
-                <Option label="Low" value="Low" />
-                <Option label="Somewhat Low" value="Somewhat Low" />
-                <Option label="Okay" value="Okay" />
-                <Option label="Somewhat High" value="Somewhat High" />
-                <Option label="High" value="High" />
-            </Select>
-        </CustomFieldEdit>
-    );
+  return (
+    <CustomFieldEdit
+      onSubmit={onSubmit}
+      header="Tell us about your motivation?"
+      width="medium"
+    >
+      <Select label="Rate how motivated you were" name="myMotivationRating">
+        <Option label="Low" value="Low" />
+        <Option label="Somewhat Low" value="Somewhat Low" />
+        <Option label="Okay" value="Okay" />
+        <Option label="Somewhat High" value="Somewhat High" />
+        <Option label="High" value="High" />
+      </Select>
+    </CustomFieldEdit>
+
+    // TODO: add checkbox group for motivators and demotivators
+  );
 };
 
-export const getMotivationRatings = async function(req) {
-    var jql = `project in (${req.context.extension.project.key})`;
-    const response = await api.asApp().requestJira(route`/rest/api/3/search?${jql}`);
-    const data = await response.json();
+export const getMotivationRatings = async function (req) {
+  var jql = `project in (${req.context.extension.project.key})`;
+  const response = await api
+    .asApp()
+    .requestJira(route`/rest/api/3/search?${jql}`);
+  const data = await response.json();
 
-    const customFieldID = await getCustomFieldID(data, 'motivationScore');
-    
-    var motivationsCount = {};
-    for (var issue of data.issues) {
-        var issueMotivationField = issue.fields[`${customFieldID}`];
-        for (const property in issueMotivationField) {
-            if (property != 'myMotivationRating' && issueMotivationField[`${property}`]) {
-                const previous = motivationsCount[`${property}`] ? motivationsCount[`${property}`] : 0;
-                motivationsCount[`${property}`] = previous + 1;
-            }
-        }
+  const customFieldID = await getCustomFieldID(data, "motivationScore");
+
+  var motivationsCount = {};
+  for (var issue of data.issues) {
+    var issueMotivationField = issue.fields[`${customFieldID}`];
+    for (const property in issueMotivationField) {
+      if (
+        property != "myMotivationRating" &&
+        issueMotivationField[`${property}`]
+      ) {
+        const previous = motivationsCount[`${property}`]
+          ? motivationsCount[`${property}`]
+          : 0;
+        motivationsCount[`${property}`] = previous + 1;
+      }
     }
+  }
 
-    return motivationsCount;
-}
+  return motivationsCount;
+};
 
-export const renderViewMotivation = render(<ViewMotivation/>);
-export const renderEditMotivation = render(<EditMotivation/>);
+export const renderViewMotivation = render(<ViewMotivation />);
+export const renderEditMotivation = render(<EditMotivation />);
