c8da79e1adb493c0ff0eedbef162fffe595c83ff
Jie <jxiangfan@gmail.com>
2022-09-24 19:20:06 +1000

    add mot/demot titles to display on chart

--- src/motivation.js
+++ src/motivation.js
@@ -25,6 +25,10 @@ const ViewMotivation = () => {
   );
 };
 
+/**
+ * Description of motivators, to show in the list of checkboxes.
+ * @enum {string}
+ */
 const motivatorsDescription = {
   m1: "Rewards and incentives (e.g. scope for increased pay and benefits linked to performance)",
   m2: "Development needs addressed (e.g. training opportunities to widen skills; opportunity to specialise)",
@@ -49,6 +53,10 @@ const motivatorsDescription = {
   m21: "Sufficient resources",
 };
 
+/**
+ * Description of de-motivators, to show in the list of checkboxes.
+ * @enum {string}
+ */
 const deMotivatorsDescription = {
   d1: "Risk",
   d2: "Stress",
@@ -67,15 +75,71 @@ const deMotivatorsDescription = {
   d15: "Lack of influence/not involved in decision making/no voice",
 };
 
-const EditMotivation = () => {
-  const onSubmit = (formData) => {
-    // sample-output formData = {
-    //   myMotivationRating: 'Somewhat High',
-    //   motivators: [ 'm1', 'm2', 'm3' ],
-    //   deMotivators: [ 'd1', 'd2', 'd3' ]
-    // }
+/**
+ * Title of motivators, to show in the radar chart.
+ * @enum {string}
+ */
+const motivatorsTitle = {
+  m1: "Rewards and incentives",
+  m2: "Development needs addressed",
+  m3: "Variety of work",
+  m4: "Career path",
+  m5: "Empowerment",
+  m6: "Good management",
+  m7: "Sense of belonging",
+  m8: "Work/life balance",
+  m9: "Working in successful company",
+  m10: "Employee involvement",
+  m11: "Feedback",
+  m12: "Recognition",
+  m13: "Equity",
+  m14: "Trust",
+  m15: "Technically challenging work",
+  m16: "Job security",
+  m17: "Identify with the task",
+  m18: "Autonomy",
+  m19: "Appropriate working conditions",
+  m20: "Task significance",
+  m21: "Sufficient resources",
+};
 
+/**
+ * Title of de-motivators, to show in the radar chart.
+ * @enum {string}
+ */
+const deMotivatorsTitle = {
+  d1: "Risk",
+  d2: "Stress",
+  d3: "Inequity",
+  d4: "Interesting work going to other parties",
+  d5: "Unfair reward system",
+  d6: "Lack of promotion opportunities",
+  d7: "Poor communication",
+  d8: "Uncompetitive pay",
+  d9: "Unrealistic goals",
+  d10: "Bad relationship with users and colleagues",
+  d11: "Poor working environment",
+  d12: "Poor management",
+  d13: "Producing poor quality software",
+  d14: "Poor cultural fit",
+  d15: "Lack of influence",
+};
+
+const EditMotivation = () => {
+  /**
+   * @param {*} formData
+   *    sample formData = {
+   *      myMotivationRating: 'Somewhat High',
+   *      motivators: [ 'm1', 'm2', 'm3' ],
+   *      deMotivators: [ 'd1', 'd2', 'd3' ]
+   *    }
+   * @returns
+   */
+  const onSubmit = (formData) => {
+    // motivation score
     formData.motivationScore = false;
+
+    // motivators
     formData.challenging = true;
     formData.interesting = true;
     formData.important = true;
--- static/manager-dashboard/src/Components/Motivation.js
+++ static/manager-dashboard/src/Components/Motivation.js
@@ -1,34 +1,38 @@
-import React, { useEffect, useState } from 'react';
-import { invoke } from '@forge/bridge';
-import RadarChart from '../shared/RadarChart';
-import './Motivation.css';
+import React, { useEffect, useState } from "react";
+import { invoke } from "@forge/bridge";
+import RadarChart from "../shared/RadarChart";
+import "./Motivation.css";
 
 function Motivation() {
-    const [data, setData] = useState(null);
+  const [data, setData] = useState(null);
 
-    useEffect(() => {
-        invoke('getMotivation', { example: 'my-invoke-variable' }).then(setData);
-    }, []);
+  useEffect(() => {
+    invoke("getMotivation", { example: "my-invoke-variable" }).then(setData);
+  }, []);
 
-    const labels = [];
-    const dataPoints = [];
-    for (const property in data) {
-        labels.push(property);
-        dataPoints.push(data[`${property}`]);
-    }
-    const borderColour = 'rgba(0, 82, 204)'
-    const backgroundColour = 'rgba(179, 212, 255)'
-    return (
-        <div className='page'>
-            <RadarChart
-                name='Motivation'
-                labels={labels}
-                dataPoints={dataPoints}
-                borderColour={borderColour}
-                backgroundColour={backgroundColour}
-            />
-        </div>
-    );
+  const labels = [];
+  const dataPoints = [];
+
+  console.log(data);
+
+  for (const property in data) {
+    console.log(property);
+    labels.push(property);
+    dataPoints.push(data[`${property}`]);
+  }
+  const borderColour = "rgba(0, 82, 204)";
+  const backgroundColour = "rgba(179, 212, 255)";
+  return (
+    <div className="page">
+      <RadarChart
+        name="Motivation"
+        labels={labels}
+        dataPoints={dataPoints}
+        borderColour={borderColour}
+        backgroundColour={backgroundColour}
+      />
+    </div>
+  );
 }
 
 export default Motivation;
