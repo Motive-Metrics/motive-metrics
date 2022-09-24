21ba92d0439022eaf480c4813c32c4c88a7b4f5c
Kunj Dave <kdav0006@student.monash.edu>
2022-09-24 14:59:04 +1000

    Add Satisfaction rating as an object instead of string

--- src/satisfaction.js
+++ src/satisfaction.js
@@ -1,4 +1,6 @@
 import ForgeUI, { CustomField, CustomFieldEdit, Option, render, Select, StatusLozenge, Text, useProductContext } from '@forge/ui';
+import api, {route} from '@forge/api';
+import { getCustomFieldID } from './index';
 
 const MySatisfactionView = () => {
     const getLozengeApperance = (rating) => {
@@ -22,10 +24,12 @@ const MySatisfactionView = () => {
         extensionContext: { fieldValue },
     } = useProductContext();
 
+    const output = fieldValue === null ? 'None' : fieldValue.mySatisfactionRating;
+
     return (
         <CustomField>
             <Text>
-              <StatusLozenge text={fieldValue || 'None'} appearance={getLozengeApperance(fieldValue)}></StatusLozenge>
+              <StatusLozenge text={output} appearance={getLozengeApperance(output)}></StatusLozenge>
           </Text>
         </CustomField>
     );
@@ -33,11 +37,11 @@ const MySatisfactionView = () => {
 
 const MySatisfactionEdit = () => {
   const onSubmit = (formValue) => {
-      return formValue.mySatisfactionRating;
+      return formValue;
   }
 
   return (
-      <CustomFieldEdit onSubmit={onSubmit} header="How would you rate your satisfaction of this Issue?" width="medium" >
+      <CustomFieldEdit onSubmit={onSubmit} header="How would you rate your satisfaction for this Issue?" width="medium" >
           <Select label="Select one of the options below" name="mySatisfactionRating">
               <Option label="Bad" value="Bad" />
               <Option label="Somewhat Bad" value="Somewhat Bad" />
@@ -47,6 +51,44 @@ const MySatisfactionEdit = () => {
           </Select>
       </CustomFieldEdit>
   );
+};
+
+export const getSatisfactionRatingsData = async function (req) {
+    var jql = `project in (${req.context.extension.project.key})`;
+    const response = await api.asApp().requestJira(route`/rest/api/3/search?${jql}`);
+    const data = await response.json();
+
+    const customFieldID = await getCustomFieldID(data, 'mySatisfactionRating');
+
+    var issueSatisfaction = [];
+    for (var issue of data.issues) {
+        if ( issue.fields[customFieldID].mySatisfactionRating ) {
+            issueSatisfaction.push(issue.fields[customFieldID].mySatisfactionRating);
+        };
+    }
+
+    var satisfactionData = [0, 0, 0, 0, 0];
+    for (var rating of issueSatisfaction) {
+        switch (rating) {
+            case 'Bad':
+                satisfactionData[0] += 1;
+                break;
+            case 'Somewhat Bad':
+                satisfactionData[1] += 1;
+                break;
+            case "Okay":
+                satisfactionData[2] += 1;
+                break;
+            case "Somewhat Good":
+                satisfactionData[3] += 1
+                break;
+            case "Good":
+                satisfactionData[4] += 1
+                break;
+        };
+    };
+
+    return satisfactionData;
 };
 
 export const renderSatisfactionFieldView = render(<MySatisfactionView />);
