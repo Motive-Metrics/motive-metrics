9a87ab2bc5a6b8224b900d827ee03dd2ed83f575
andrwcao <80874259+andrwcao@users.noreply.github.com>
2022-09-24 13:37:19 +1000

    Create object for submission

--- static/manager-dashboard/src/Components/PersonalityTest.js
+++ static/manager-dashboard/src/Components/PersonalityTest.js
@@ -5,30 +5,46 @@ const questions = require('../questions.json');
 function PersonalityTest() {
     const [currentIndex, setCurrentIndex] = useState(0);
 
-    console.log(questions);
+    useEffect(() => {
+        if (questions[currentIndex].value) {
+            document.getElementById(questions[currentIndex].value).checked = true;
+        }
+    },[currentIndex]);
 
     const nextQuestion = () => {
         const currentlySelected = document.querySelector('input[name="question"]:checked');
         if (!currentlySelected) {
             return;
         }
-
         questions[currentIndex].value = currentlySelected.value;
-        currentlySelected.checked = false;
 
+        currentlySelected.checked = false;
         setCurrentIndex(currentIndex + 1);
-        
-        if (questions[currentIndex + 1].value) {
-            document.getElementById(questions[currentIndex + 1].value).checked = true;
-        }
     }
 
     const previousQuestion = () => {
         setCurrentIndex(currentIndex - 1);
-        
-        if (questions[currentIndex - 1].value) {
-            document.getElementById(questions[currentIndex - 1].value).checked = true;
+    }
+    
+    const submit = () => {
+        const currentlySelected = document.querySelector('input[name="question"]:checked');
+        if (!currentlySelected) {
+            return;
         }
+        questions[currentIndex].value = currentlySelected.value;
+
+        const personality = {};
+        for (const question of questions) {
+            if (!personality.hasOwnProperty(question.domain)) {
+                personality[question.domain] = {};
+                personality[question.domain].total = parseInt(0);
+            }
+
+            const value = parseInt(question.value);
+            personality[question.domain].total += question.key === "+" ? value: - value;
+            personality[question.domain].facet = question.key === "+" ? value: - value;
+        }
+        console.log(personality);
     }
 
     return (
@@ -37,18 +53,18 @@ function PersonalityTest() {
             <h3>Question { currentIndex + 1 }</h3>
             <p> {questions[currentIndex].question } </p>
             <form id="form">
-                <input type="radio" name="question" value="Never" id="Never"/>
+                <input type="radio" name="question" value="1" id="Never"/>
                 <label for="Never">Never</label>
-                <input type="radio" name="question" value="Rarely" id="Rarely"/>
+                <input type="radio" name="question" value="2" id="Rarely"/>
                 <label for="Rarely">Rarely</label>
-                <input type="radio" name="question" value="Sometimes" id="Sometimes"/>
+                <input type="radio" name="question" value="3" id="Sometimes"/>
                 <label for="Sometimes">Sometimes</label>
-                <input type="radio" name="question" value="Often" id="Often"/>
+                <input type="radio" name="question" value="4" id="Often"/>
                 <label for="Often">Very Often</label>
-                <input type="radio" name="question" value="Always" id="Always"/>
+                <input type="radio" name="question" value="5" id="Always"/>
                 <label for="Always">Always</label>
             </form>
-
+            
             {currentIndex > 0 &&
                 <button id="previous" onClick={previousQuestion}>Previous</button>
             }
@@ -56,7 +72,7 @@ function PersonalityTest() {
                 <button id="next" onClick={nextQuestion}>Next</button>
             }
             {currentIndex === 119 &&
-                <button id="submit">Submit</button>
+                <button id="submit" onClick={submit}>Submit</button>
             }
         </div>
     );
