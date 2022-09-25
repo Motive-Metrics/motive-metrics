4e6392006d54b6b137d10bfeb13a969c9b42b52b
Sharan Sharabinth <ssha0054@student.monash.edu>
2022-09-25 19:04:31 +1000

    Partially implement peer assess performance functionality

--- src/peerRatedPerformance.js
+++ src/peerRatedPerformance.js
@@ -1,8 +1,26 @@
-import ForgeUI, { render, CustomField, CustomFieldEdit, Select, Option, useProductContext, Text, StatusLozenge } from '@forge/ui';
+import ForgeUI, { render, CustomField, CustomFieldEdit, Select, Option, useProductContext, useState, Text, StatusLozenge } from '@forge/ui';
 import api, {route} from '@forge/api';
 import { getCustomFieldID } from './index';
 
 const PeerAssessedPerformanceView = () => {
+    
+    const {platformContext: {issueKey}} = useProductContext();
+    const [assigneeId] = useState(getIssueAssigneeID(issueKey));
+    const {accountId} = useProductContext();
+    
+    // If current user logged in == assignee ID, then display “Cannot peer rate yourself”
+    if (accountId == assigneeId) {
+        return (
+            <CustomField>
+                <Text>Cannot peer rate yourself!</Text>
+            </CustomField>
+        )
+    };
+
+    // If ID of current user logged in does not exist in return value, then display “Peer assess performance”
+
+    // Else, display the rating as a StatusLozenge
+    
     const getLozengeApperance = (rating) => {
         switch (rating) {
             case 'Good':
@@ -24,7 +42,7 @@ const PeerAssessedPerformanceView = () => {
         extensionContext: { fieldValue },
     } = useProductContext();
 
-    const output = fieldValue === null ? 'None' : fieldValue.myPerformanceRating;
+    const output = fieldValue === null ? 'None' : fieldValue.peerAssessedPerformanceRating;
 
     return (
         <CustomField>
@@ -35,13 +53,54 @@ const PeerAssessedPerformanceView = () => {
     );
 };
 
+
 const PeerAssessedPerformanceEdit = () => {
+    // get current ID
+    const {platformContext: {issueKey}} = useProductContext();
+    // console.log(issueKey);
+
+    const [assigneeId] = useState(getIssueAssigneeID(issueKey));
+    // console.log(assigneeId);
+    
+    const {accountId} = useProductContext();
+    // console.log(accountId);
+
+    // If current user is the assignee, don't let them peer assess. Need to return CustomFieldEdit in here though
+    if (accountId == assigneeId) {
+        
+        const onSubmit = (formValue) => {
+            
+            console.log(formValue);
+            // Need to return something, currently returning -1 with this usersId.  
+            return {
+                "accountId": accountId,
+                "peerAssessedPerformanceRating": '-1'
+            };
+        }
+        
+        return (
+            <CustomFieldEdit onSubmit={onSubmit} header="How would you rate the performance of your peer?" width="medium" >
+                <Text>Cannot peer assess yourself!</Text>
+            </CustomFieldEdit>
+        );
+    };
+
+    // Else, let user rate performance like self assessment 
     const onSubmit = (formValue) => {
-        return formValue;
+        // Right now only 1 Object is stored. Need to update to be a list.
+
+        // Need to iterate list and check if accountId exists in list
+        // If ID exists, update teh performance rating value
+        // Else, push a new object for this user
+        
+        return {
+            "accountId": accountId,
+            "peerAssessedPerformanceRating": formValue.myPerformanceRating
+        };
     }
 
     return (
-        <CustomFieldEdit onSubmit={onSubmit} header="How would you rate your performance?" width="medium" >
+        <CustomFieldEdit onSubmit={onSubmit} header="How would you rate the performance of your peer?" width="medium" >
             <Select label="Select one of the options below to rate your performance" name="myPerformanceRating">
                 <Option label="Bad" value="Bad" />
                 <Option label="Somewhat Bad" value="Somewhat Bad" />
@@ -53,7 +112,19 @@ const PeerAssessedPerformanceEdit = () => {
     );
 };
 
-export const getPerformanceRatingsData = async function (req) {
+const getIssueAssigneeID = async function (issueKey) {
+    const response = await api.asApp().requestJira(route`/rest/api/3/issue/${issueKey}`);
+    const data = await response.json();
+    
+    var assigneeId = null;
+    if ( data.fields.assignee ) {
+        assigneeId = data.fields.assignee.accountId;
+    }
+    
+    return assigneeId;
+};
+
+export const getPeerAssessedPerformanceRatings = async function (req) {
     var jql = `project in (${req.context.extension.project.key})`;
     const response = await api.asApp().requestJira(route`/rest/api/3/search?${jql}`);
     const data = await response.json();
